24ecfbdc3f32c603298f7115294ee030d0d54ea4
俞宏伟 <31241366+githowe@users.noreply.github.com>
2025-08-13 07:58:50 +0800

    删除废弃资源 因为控制右侧面板显隐功能已失效，所以更新了默认缓存，默认右面板为关闭状态

--- GeekDocument/SubSystem/CacheSystem/Define/MainWindowCache.cs
+++ GeekDocument/SubSystem/CacheSystem/Define/MainWindowCache.cs
@@ -12,7 +12,7 @@ namespace GeekDocument.SubSystem.CacheSystem.Define
         public int Height { get; set; } = 900;
 
         public bool LeftPanelHided { get; set; } = false;
-        public bool RightPanelHided { get; set; } = false;
+        public bool RightPanelHided { get; set; } = true;
 
         public int LeftPanelWidth { get; set; } = 300;
         public int RightPanelWidth { get; set; } = 300;
